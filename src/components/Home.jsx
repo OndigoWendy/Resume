3540d2a1465664945816bcf631d57b2cc9483a19
Wendy <ondigowendy004@gmail.com>
2023-01-02 15:29:13 +0300

    Remove all maps implementation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,8 +4,7 @@ import Fade from 'react-reveal';
 import endpoints from '../constants/endpoints';
 import Social from './Social';
 import FallbackSpinner from './FallbackSpinner';
-import { useLoadScript } from '@react-google-maps/api';
-import { GoogleMap, Marker } from '@react-google-maps/api';
+
 
 const styles = {
   nameStyle: {
@@ -26,36 +25,7 @@ const styles = {
  function Home() {
   const [data, setData] = useState(null);
 
-  const libraries = ['places'];
-  const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: 'AIzaSyAvNcw4PwN72DeAzyvhn9AOFh9t2vII9VE',
-    libraries,
-  });
-
-  function renderMap() {
-    if (loadError) return 'Error loading the map';
-    if (!isLoaded) return 'Loading the map';
-
-    const mapContainerStyle = {
-      width: '400px',
-      height: '400px',
-    };
-
-    const center = {
-      lat: -34.397,
-      lng: 150.644,
-    };
-
-    return (
-      <GoogleMap
-        mapContainerStyle={mapContainerStyle}
-        zoom={8}
-        center={center}
-      >
-        <Marker position={center} />
-      </GoogleMap>
-    );
-  }
+ 
 
   useEffect(() => {
     fetch(endpoints.home, {
@@ -82,9 +52,7 @@ const styles = {
         </div>
         <Social />
       </div>
-      {/* <div>
-        {renderMap()}
-      </div> */}
+      
     </Fade>
   ) : <FallbackSpinner />;
           }
